f258cde6410377753e740ec391264e4908acc946
sirabgehoben <admin@abgehoben.org>
2026-09-10 19:25:06 +0200

    Add a centralized Dimens token system for magic numbers. Semantic domain colors have been centralized in Color.kt. these are not being used for now but will be implemented in the future

--- app/src/main/java/org/abgehoben/xenon/ui/theme/Shape.kt
+++ app/src/main/java/org/abgehoben/xenon/ui/theme/Shape.kt
@@ -2,12 +2,11 @@ package org.abgehoben.xenon.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
-import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    extraSmall = RoundedCornerShape(8.dp),
-    small = RoundedCornerShape(12.dp),
-    medium = RoundedCornerShape(16.dp),
-    large = RoundedCornerShape(24.dp),
-    extraLarge = RoundedCornerShape(32.dp)
+    extraSmall = RoundedCornerShape(Dimens.RadiusExtraSmall),
+    small = RoundedCornerShape(Dimens.RadiusMedium),
+    medium = RoundedCornerShape(Dimens.RadiusLarge),
+    large = RoundedCornerShape(Dimens.RadiusDialog),
+    extraLarge = RoundedCornerShape(Dimens.RadiusExtraLarge)
 )
